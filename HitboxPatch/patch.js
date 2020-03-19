9432ad2233c8286b4b334970175b7a5cf3eabf8d
ChrisGrando <47995783+chrisGrando@users.noreply.github.com>
2020-03-19 19:54:52 -0300

    Initial Commit

---
+++ HitboxPatch/patch.js
@@ -0,0 +1,11 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+	"id": "HitboxPatch",
+	"servers": [
+		"https://raw.githubusercontent.com/chrisGrando/EoSD-Hitbox/master/HitboxPatch/"
+	],
+	"title": "EoSD Always Visible hitbox. Ported from Hitbox Patch v0.1",
+	"update": false
+}
---
+++ repo.js
@@ -0,0 +1,11 @@
+{
+	"contact": "christianjulio2010@gmail.com",
+	"id": "kriChris",
+	"patches": {
+		"HitboxPatch": "EoSD Always Visible hitbox. Ported from Hitbox Patch v0.1"
+	},
+	"servers": [
+		"https://raw.githubusercontent.com/chrisGrando/EoSD-Hitbox/master/"
+	],
+	"title": "Patches by kriChris"
+}
