f5dc8974fc0714c4b6191ab05d979429649f824f
ChrisGrando <47995783+chrisGrando@users.noreply.github.com>
2020-03-25 17:29:27 -0300

    Inserted tab

--- repo.js
+++ repo.js
@@ -2,7 +2,7 @@
 	"contact": "christianjulio2010@gmail.com",
 	"id": "kriChris",
 	"patches": {
-		"Auto-Bomb": "Automatically triggers a bomb as soon as you got hit.\n Supports Touhou 06, 07, 08, 10, 11, 12, 12.8, 13, 14, 15, 16, 17 and Uwabami Breakers.",
+		"Auto-Bomb": "Automatically triggers a bomb as soon as you got hit.\n\tSupports Touhou 06, 07, 08, 10, 11, 12, 12.8, 13, 14, 15, 16, 17 and Uwabami Breakers.",
 		"HitboxPatch": "EoSD always visible hitbox. Ported from Hitbox Patch v0.1"
 	},
 	"servers": [
