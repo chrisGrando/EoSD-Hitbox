d3c3481ac43968ec835c057c32ae9dc87db0d8cd
chrisGrando <christianjulio2010@gmail.com>
2021-05-30 20:54:54 -0300

    New mod (WIP)

---
+++ 8Bosses/files.js
@@ -0,0 +1,12 @@
+{
+	"patch.js": 2349492545,
+	"th08.v1.00d.js": 719533320,
+	"th08/ecldata1.ecl": 2228581166,
+	"th08/ecldata2.ecl": 928897874,
+	"th08/ecldata3.ecl": 403789789,
+	"th08/ecldata4a.ecl": 208208764,
+	"th08/ecldata4b.ecl": 477879205,
+	"th08/ecldata5.ecl": 3239322928,
+	"th08/ecldata6.ecl": 3644856489,
+	"th08/ecldata7.ecl": 4029790006
+}
